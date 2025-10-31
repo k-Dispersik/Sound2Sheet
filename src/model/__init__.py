306b9fe62f41aca02c947e356d80a6c02d70d00b
Volodymyr M. <harvel.bk@gmail.com>
2025-10-31 18:09:00 +0200

    Add model package exports

---
+++ src/model/__init__.py
@@ -0,0 +1,28 @@
+"""
+Model package for Sound2Sheet.
+
+Contains the model architecture, training, and inference code.
+"""
+
+from .config import ModelConfig, TrainingConfig, InferenceConfig, DataConfig
+from .sound2sheet_model import Sound2SheetModel
+from .ast_model import ASTWrapper, NoteDecoder
+from .dataset import PianoDataset, create_dataloaders
+from .trainer import Trainer
+
+# Scripts are importable but not in __all__ (they have main() functions)
+from . import train
+from . import inference
+
+__all__ = [
+    'ModelConfig',
+    'TrainingConfig', 
+    'InferenceConfig',
+    'DataConfig',
+    'Sound2SheetModel',
+    'ASTWrapper',
+    'NoteDecoder',
+    'PianoDataset',
+    'create_dataloaders',
+    'Trainer',
+]
