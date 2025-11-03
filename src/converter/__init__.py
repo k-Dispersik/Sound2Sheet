448fab3373367576bb913a1d8311a1bc537aa383
Volodymyr M. <harvel.bk@gmail.com>
2025-11-03 14:36:40 +0200

    feat: export new classes (Dynamic, Articulation, Measure, MusicXMLConverter)

--- src/converter/__init__.py
+++ src/converter/__init__.py
@@ -4,18 +4,22 @@ Converter module for Sound2Sheet.
 Converts model predictions into structured musical notation formats (JSON, MIDI).
 """
 
-from .note import Note
-from .note_sequence import NoteSequence
+from .note import Note, Dynamic, Articulation
+from .note_sequence import NoteSequence, Measure
 from .note_builder import NoteBuilder
 from .quantizer import QuantizationConfig, Quantizer
-from .converter import MIDIConverter, JSONConverter
+from .converter import MIDIConverter, JSONConverter, MusicXMLConverter
 
 __all__ = [
     'Note',
+    'Dynamic',
+    'Articulation',
     'NoteSequence',
+    'Measure',
     'NoteBuilder',
     'QuantizationConfig',
     'Quantizer',
     'MIDIConverter',
     'JSONConverter',
+    'MusicXMLConverter',
 ]
